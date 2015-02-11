a29453787d20546f8755ebbfac1029d60ac6df53
june <kjune.bae@gmail.com>
2015-02-11 15:19:37 +0900

    .

--- web01/src/com/kdn/web/servlet/BoardInsert.java
+++ web01/src/com/kdn/web/servlet/BoardInsert.java
@@ -1,6 +1,7 @@
 package com.kdn.web.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
@@ -14,19 +15,29 @@ public class BoardInsert extends GenericServlet {
   @Override
   public void service(ServletRequest request, ServletResponse response)
       throws ServletException, IOException {
+    //한글이 깨지지 않도록 하려면, getParameter()를 최초로 호출하기 전에 
+    //입력 데이터의 인코딩 형식을 설정해야 한다.
+    request.setCharacterEncoding("UTF-8");
     String title = request.getParameter("title");
     String content = request.getParameter("content");
     
-    System.out.println(title);
-    System.out.println(content);
+    //한글을 출력하기
+    //- 출력 스트림을 얻기 전에, 전송할 데이터의 형식과 문자집합을 설정한다.
+    response.setContentType("text/html;charset=UTF-8");
     
+    // 웹 브라우저로 출력하기
+    PrintWriter out = response.getWriter();
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>등록결과</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>등록 성공입니다!</h1>");
+    out.println("</body>");
+    out.println("</html>");
   }
 
 }
-
-
-
-
 
 
 
