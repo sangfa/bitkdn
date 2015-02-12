61fcdf1e3a806ae06df58075eaa11b2942e6bfb7
june <kjune.bae@gmail.com>
2015-02-12 13:52:34 +0900

    .

--- web01/src/com/kdn/web/servlet/BoardList.java
+++ web01/src/com/kdn/web/servlet/BoardList.java
@@ -1,5 +1,55 @@
 package com.kdn.web.servlet;
 
-public class BoardList {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/list")
+public class BoardList extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+    out.println("<html><head><title>게시물목록</title></head><body>");
+    out.println("<h1>게시물 목록</h1>");
+    
+    Connection con = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    
+    try {
+      Class.forName("com.mysql.jdbc.Driver");
+      con = DriverManager.getConnection(
+          /* JDBC URL */ "jdbc:mysql://localhost:3306/kdndb",
+          /* User ID */ "kdn",
+          /* User Password */ "123456789");
+      stmt = con.createStatement();
+      rs = stmt.executeQuery("select bno, title, create_date, view from boards");
+      while(rs.next()) { //DBMS 서버에서 레코드를 하나 가져온다.
+        out.println(rs.getInt("bno") + ","
+            + rs.getString("title") + ","
+            + rs.getDate("create_date") + ","
+            + rs.getInt("view") + "<br>");
+      }
+      out.println("</body></html>");
+      
+    } catch (Exception e) {
+      e.printStackTrace();
+      
+    } finally {
+      try {stmt.close();} catch (Exception e) {}
+      try {con.close();} catch (Exception e) {}
+    }
+
+  }
 }
